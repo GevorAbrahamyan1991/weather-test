443514c50fbcc7a46039eb9e52352e838a6935f1
GevorAbrahamyan1991 <abgevor@gmail.com>
2025-01-15 12:35:03 +0400

    Installl React router dom , and make code change in App . Index tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+// packages
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+// Pages
+import Home from "./Pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
